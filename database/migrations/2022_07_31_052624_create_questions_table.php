97271e35ab8cfc3511b00e8b688622af81cef1cb
KyrianCodez <egan605@gmail.com>
2022-07-31 02:08:38 -0400

    Added form and question models and migrations

---
+++ database/migrations/2022_07_31_052624_create_questions_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use phpDocumentor\Reflection\Types\Nullable;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreignId('form_id')->constrained()->onDelete('cascade');
+            $table->string('title');
+            $table->string('type');
+            $table->json('options')->nullable();
+            $table->string('value')->nullable();
+            $table->boolean('required')->default(false);
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+};
